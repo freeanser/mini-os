6d505c5b02eaded8bdfbf1a016c03ca1bc9cb382
freeanser <bnn01615@gmail.com>
2025-10-02 19:08:43 -0700

    Implement createFile function

--- FileSystem.cpp
+++ FileSystem.cpp
@@ -7,6 +7,14 @@
 namespace fs = std::filesystem;
 
 void FileSystem::createFile(const std::string& fileName, const std::string& content) {
+    std::ofstream out(fileName);
+    if (!out) {
+        std::cerr << "Cannot create file: " << fileName << "\n";
+        return;
+    }
+    out << content << "\n";
+    std::cout << "Created file: " << fileName << "\n";
+
 }
 
 void FileSystem::readFile(const std::string& fileName) {
